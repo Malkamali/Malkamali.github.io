202fd1bb940b28e68c3026e0a57f75daac080dd2
Malkamali <46822452+Malkamali@users.noreply.github.com>
2024-12-02 00:41:09 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 // src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
 import Projects from './components/Projects';
 import Footer from './components/Footer';
+import DoublePendulum from './components/DoublePendulum';
+import NNapproximation from './components/NNapproximation';
+
+// Scroll to the section based on the route or hash
+function ScrollToSection() {
+  const location = useLocation();
+
+  useEffect(() => {
+    const sectionId = location.pathname.split('/').pop(); // Get the last part of the URL
+    const element = document.getElementById(sectionId);
+
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, [location]);
+
+  return null;
+}
 
 function App() {
   return (
     <Router>
+      <ScrollToSection />
       <Navbar />
       <Routes>
         <Route 
@@ -16,15 +35,13 @@ function App() {
           element={
             <>
               <HeroSection />
-              <div id="double-pendulum">
-                <Projects />
-              </div>
-              <div id="neural-network-approximation">
-                <Projects />
-              </div>
+              <Projects />
             </>
           } 
         />
+        {/* Specific project routes */}
+        <Route path="/projects/doublependulum" element={<DoublePendulum />} />
+        <Route path="/projects/nnapproximation" element={<NNapproximation />} />
       </Routes>
       <Footer />
     </Router>
